76a462471e28d961e9f572c9472fefd823803428
Jakob Hallin <jakob.l.hallin@gmail.com>
2025-07-30 07:27:18 -0700

    Building supply depot up to 3

--- simplebot.py
+++ simplebot.py
@@ -4,6 +4,7 @@ from sc2.data import Race, Difficulty
 from sc2.main import run_game
 from sc2.player import Bot, Computer
 from sc2.ids.unit_typeid import UnitTypeId
+from sc2.position import Point2
 
 
 class MyBot(BotAI):
@@ -15,8 +16,19 @@ class MyBot(BotAI):
                     self.do(cc.train(UnitTypeId.SCV))
                     print("Train SCV")
 
+        total_depots = self.structures(UnitTypeId.SUPPLYDEPOT).amount + self.already_pending(UnitTypeId.SUPPLYDEPOT)
+        if self.structures(UnitTypeId.SUPPLYDEPOT).amount + self.already_pending(UnitTypeId.SUPPLYDEPOT) < 4:
+            if self.can_afford(UnitTypeId.SUPPLYDEPOT):
+                worker = self.workers.random_or(None)
+                if worker:
+                    base = self.townhalls.first.position
+                    offset_x = 4 + (total_depots * 2)
+                    build_pos = Point2((base.x + offset_x, base.y))
+                    self.do(worker.build(UnitTypeId.SUPPLYDEPOT, build_pos))
+                    print(f"Building Supply Depot #{total_depots + 1} at {build_pos.rounded}")
+
 run_game(
-    maps.get("Flat32"),  # You can replace with any valid map
+    maps.get("Flat64"),  # You can replace with any valid map
     [Bot(Race.Terran, MyBot()), Computer(Race.Zerg, Difficulty.Easy)],
     realtime=False,
 )
