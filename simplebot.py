70b2b81a1e44e87c8b2e046531d10519e14552c6
Jakob Hallin <jakob.l.hallin@gmail.com>
2025-07-30 07:11:04 -0700

    Train scvs

--- simplebot.py
+++ simplebot.py
@@ -3,11 +3,17 @@ from sc2.bot_ai import BotAI
 from sc2.data import Race, Difficulty
 from sc2.main import run_game
 from sc2.player import Bot, Computer
+from sc2.ids.unit_typeid import UnitTypeId
 
 
 class MyBot(BotAI):
     async def on_step(self, iteration):
-        print(f"Step {iteration}")
+        #print(f"Step {iteration}")
+        if self.townhalls:
+            cc = self.townhalls.first
+            if self.can_afford(UnitTypeId.SCV) and cc.is_idle:
+                    self.do(cc.train(UnitTypeId.SCV))
+                    print("Train SCV")
 
 run_game(
     maps.get("Flat32"),  # You can replace with any valid map
