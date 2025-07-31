4939c4f38d9e570aefbd0838f920e8e576ebe35b
Jakob Hallin <jakob.l.hallin@gmail.com>
2025-07-31 08:08:55 -0700

    Add d refinery filling

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sc2.ids.upgrade_id import UpgradeId
 from tasks.infrastucture import build_supply_depots, build_barracks, build_orbiltal_command, expand, build_techlab, build_refinery
 from tasks.actions import scout_enemy_base, attack_enemy
 from tasks.research import research_upgrades_stimpack
-from tasks.units import build_workers, build_marines, build_marauders
+from tasks.units import build_marines, build_marauders
 class MyBot(BotAI):
     def __init__(self):
         self.scout_unit = None
@@ -24,7 +24,15 @@ class MyBot(BotAI):
         self.techlab_built = False
 
 
+    async def build_workers(bot):
+        #if bot.townhalls:
+        cc = bot.townhalls.first
+        ideal_workers = 22#sum(townhall.ideal_harvesters for townhall in bot.townhalls)+1
 
+        if bot.can_afford(UnitTypeId.SCV) and cc.is_idle and bot.workers.amount < ideal_workers:
+            bot.do(cc.train(UnitTypeId.SCV))   
+            print(f"Train SCV: {bot.workers.amount} / {ideal_workers}")
+            
 
     async def calldown_mule(self):
         if self.structures(UnitTypeId.ORBITALCOMMAND).ready.exists:
@@ -33,7 +41,15 @@ class MyBot(BotAI):
             if self.can_afford(AbilityId.CALLDOWNMULE_CALLDOWNMULE):
                 self.do(oc(AbilityId.CALLDOWNMULE_CALLDOWNMULE, target_mineral))
                 print("Calling down MULE")
-
+    async def fill_refineries(bot):
+        for refinery in bot.gas_buildings.ready:
+            assigned = refinery.assigned_harvesters
+            ideal = refinery.ideal_harvesters
+            if assigned < ideal:
+                for _ in range(ideal - assigned):
+                    worker = bot.workers.gathering.random_or(None)
+                    if worker:
+                        bot.do(worker.gather(refinery))
 
     
                 
@@ -44,17 +60,18 @@ class MyBot(BotAI):
 
         await self.distribute_workers()
         await build_orbiltal_command(self)
-        await build_workers(self)
+        await self.build_workers()
         await build_supply_depots(self)
         await build_barracks(self)
         await build_techlab(self)
-        #await self.build_marines()
+        await build_marines(self)
         await attack_enemy(self)
         await self.calldown_mule()
         scout_enemy_base(self)
         await expand(self)
         await build_refinery(self)
         await research_upgrades_stimpack(self)
+        await self.fill_refineries()
         #await self.build_marauders()
         
         
--- tasks/units.py
+++ tasks/units.py
@@ -1,12 +1,4 @@
 from sc2.ids.unit_typeid import UnitTypeId
-async def build_workers(bot):
-    #if bot.townhalls:
-    cc = bot.townhalls.first
-    ideal_workers = sum(townhall.ideal_harvesters for townhall in bot.townhalls)+1
-
-    if bot.can_afford(UnitTypeId.SCV) and cc.is_idle and bot.workers.amount < ideal_workers:
-        bot.do(cc.train(UnitTypeId.SCV))   
-        print(f"Train SCV: {bot.workers.amount} / {ideal_workers}")
 async def build_marines(bot):
     for barracks in bot.structures(UnitTypeId.BARRACKS).ready.idle:
         
